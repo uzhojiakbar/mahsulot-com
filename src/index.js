cf1210b3831ecfdc9cf5413d4f39c5a0af478424
aziz2982 <azizbek30052982@gmail.com>
2023-06-19 18:53:10 +0500

    prodacts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Root from './Root';
+import Products from './Components/products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Root />
+      <Products/>
     </BrowserRouter>
   </React.StrictMode>
 );
